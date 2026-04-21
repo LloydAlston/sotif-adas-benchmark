5ccd31c0dd902b30c7cbf4b062a2d9ca885e4cf2
LloydAlston <lloydalston101@gmail.com>
2026-04-21 23:51:28 +0200

    feat: configure default clear weather parameters in CARLA world setup

--- world_setup/carla_world.py
+++ world_setup/carla_world.py
@@ -3,3 +3,7 @@ client= carla.Client('localhost',2000)
 client.set_timeout(15.0)
 world=client.get_world()
 print(f"connected to CARLA. MAP : {world.get_map().name}")
+weather=carla.WeatherParameters(cloudiness=0.0, precipitation=0.0, sun_altitude_angle=70.0)
+world.set_weather(weather)
+print("Weather set to clear sky, sun high in the sky")
+
